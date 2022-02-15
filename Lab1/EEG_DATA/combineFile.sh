0b2168a84dcfb233529cb8eb57629ff1db0e07c8
Grace <grace.gibson571@gmail.com>
2022-02-15 10:55:51 -0600

    FixedAndTestedBashFiles

--- Lab1/EEG_DATA/combineFile.sh
+++ Lab1/EEG_DATA/combineFile.sh
@@ -4,17 +4,17 @@
 #Concatenates all 60 data files from the user-specified concentration level into one data file
 
 #prompt user for input
-echo "Enter the concentration level to analyze (0 or 5)"
+echo "Enter the concentration level to analyze (00 or 05)"
 read conc_level #read user input
 #echo $conc_level
 touch catted_input.dat
-if test $conc_level = "0"
+if test $conc_level = "00"
 then
 #echo "entered"
 #touch catted_data_test.dat #create file to concatenate all 60 data files to
 cat ./ERP/ERP00/* > catted_data.dat #concatenate all files in a directory and output to new file
 fi
-if test $conc_level = "5"
+if test $conc_level = "05"
 then
 #touch catted_data_test2.dat #create file to concatenate all 60 data files to
 cat ./ERP/ERP05/* > catted_data.dat #concatenate all files in a directory and output to new file
@@ -22,8 +22,9 @@ fi
 #execute the histogram program for this new data set
 #cp /home/gibsong/BIEN4820/Lab1/gibsong_stats.hpp gibsong_stats.hpp
 
-g++ -c gibsong_stats.cpp
-g++ -o hist gibsong_stats_test.cpp
+make clean
+make hist
+
 touch hist$conc_level.txt
 echo "catted_data.dat" | ./hist  > hist$conc_level.txt
 #print to screen where output is stored 
---
+++ Lab1/EEG_DATA/conc_corr.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Author:  Grace Gibson
+#Date Created: 2/8/2022
+#Concatenates all 60 data files from the user-specified concentration level into one data file
+
+#prompt user for input
+echo "Enter the concentration level to analyze (00 or 05)"
+read conc_level #read user input
+
+touch conc_corr_$conc_level.csv
+
+make clean
+make all
+counter=1
+while [ $counter -le 5 ] 
+do
+    #echo "hi"
+    (( counter++ ))
+done
+counter=1
+while [ $counter -le 59 ]
+do 
+    #echo -e "hi\n"
+    if [ $conc_level = "00" ]
+    then
+        echo -e "./ERP/ERP${conc_level}/ERP${conc_level}${counter}.txt\n./ERP/ERP${conc_level}/ERP${conc_level}${counter+1}.txt\n"
+        echo -e "./ERP/ERP${conc_level}/ERP${conc_level}${counter}.txt\n./ERP/ERP${conc_level}/ERP${conc_level}${counter+1}.txt\n" | ./corr >> conc_corr_$conc_level.csv
+    else 
+        echo -e "./ERP/ERP${conc_level}/ERP${conc_level}.${counter}.txt\n./ERP/ERP${conc_level}/ERP${conc_level}.${counter+1}.txt\n" | ./corr >> conc_corr_$conc_level.csv
+    fi
+    (( counter++ ))
+done
